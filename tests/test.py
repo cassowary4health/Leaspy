9d85a811c8b15fe73b6ce860a4cf0c42fd13abcb
Symcies <koval.igor.92@gmail.com>
2019-03-15 16:27:53 +0100

    [TEST] Update tests - 13 tests passed, 0 fails

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,14 @@
 import unittest
 
 from tests.inputs.model_parameters_reader import ModelParametersReaderTest
+from tests.inputs.data_reader import DataReaderTest
+from tests.inputs.individual_data import IndividualDataTest
+from tests.inputs.data import DataTest
+
 from tests.models.model_factory import ModelFactoryTest
 from tests.models.abstract_model import AbstractModelTest
 from tests.models.univariate_model import UnivariateModelTest
 from tests.main.main import LeaspyTest
+
 
 unittest.main()

e9f37bfe79614fb967a5c80e2a8002647792e2dc
Raphael <raphael.couronne@gmail.com>
2019-03-15 20:41:22 +0100

    [FEAT] Added attachment to GaussianDistributionModel

--- src/models/gaussian_distribution_model.py
+++ src/models/gaussian_distribution_model.py
@@ -4,6 +4,9 @@ from src import default_data_dir
 from src.models.abstract_model import AbstractModel
 from src.inputs.model_parameters_reader import ModelParametersReader
 
+import torch
+from torch.autograd import Variable
+
 
 class GaussianDistributionModel(AbstractModel):
     def __init__(self):
@@ -13,3 +16,17 @@ class GaussianDistributionModel(AbstractModel):
         if reader.model_type != 'gaussian_distribution':
             raise ValueError("The default univariate parameters are not of gaussian_distribution type")
 
+        # TODO to Pytorch
+        self.model_parameters['mu'] = Variable(torch.tensor(self.model_parameters['mu']).float(), requires_grad=True)
+
+    def compute_individual(self, indiviual):
+        return self.model_parameters['mu'] + indiviual.individual_parameters['a']*torch.ones_like(indiviual.tensor_timepoints)
+
+    def compute_attachment(self, data):
+        likelihood = 0
+        for individual in data:
+            likelihood += self.compute_individual_fit_likelihood(individual)
+        return likelihood
+
+    def compute_individual_attachment(self, individual):
+         return torch.sum((self.compute_individual(individual)-individual.tensor_observations)**2)

79b98e0cd7adf2b5f06193811114cf7cf0cf8a9d
Raphael <raphael.couronne@gmail.com>
2019-03-17 12:59:09 +0100

    [FEAT] Gradient Descent

---
+++ src/algo/gradient_descent.py
@@ -0,0 +1,40 @@
+import torch
+from src.algo.abstract_algo import AbstractAlgo
+import os
+from src.inputs.algo_reader import AlgoReader
+from src import default_algo_dir
+
+
+class GradientDescent(AbstractAlgo):
+
+    def __init__(self):
+        data_dir = os.path.join(default_algo_dir, "default_gradient_descent_parameters.json")
+        reader = AlgoReader(data_dir)
+
+        if reader.algo_type != 'gradient_descent':
+            raise ValueError("The default gradient descent parameters are not of gradient_descent type")
+
+        self.parameters = reader.parameters
+
+    def run(self, data, model):
+        for iter in range(self.parameters['n_iter']):
+            self.iter(data, model)
+
+    def iter(self, data, model):
+        # Compute loss
+        attachment = model.compute_attachment(data)
+        regularity = 0
+        loss = attachment + regularity
+
+        # Do backward
+        loss.backward()
+        with torch.no_grad():
+            for key in model.model_parameters:
+                # TODO not model specific optimization of parameters : sigma ? When grad is none
+                if model.model_parameters[key].grad is not None:
+                    model.model_parameters[key] -= self.parameters['learning_rate'] * model.model_parameters[key].grad
+                    model.model_parameters[key].grad.zero_()
+        return 0
+
+
+

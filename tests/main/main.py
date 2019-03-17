a96f60aaa768a33a46dc1cdd338c61569037a253
Raphael <raphael.couronne@gmail.com>
2019-03-17 13:06:04 +0100

    [TEST] Mu estimation of Gaussian Distribution Model

--- tests/main/main.py
+++ tests/main/main.py
@@ -5,6 +5,15 @@ from tests import test_data_dir
 from src.main import Leaspy
 from src.models.univariate_model import UnivariateModel
 from src.inputs.model_parameters_reader import ModelParametersReader
+from src.inputs.data_reader import DataReader
+from src.inputs.data import Data
+
+from src.algo.algo_factory import AlgoFactory
+from src.inputs.algo_reader import AlgoReader
+
+from src.algo.gradient_descent import GradientDescent
+
+import torch
 
 
 class LeaspyTest(unittest.TestCase):
@@ -28,3 +37,16 @@ class LeaspyTest(unittest.TestCase):
         self.assertEqual(leaspy.model.model_parameters['tau_std'], 2)
         self.assertEqual(leaspy.model.model_parameters['xi_mean'], -10)
         self.assertEqual(leaspy.model.model_parameters['xi_std'], 0.8)
+
+
+    def test_gaussian_distribution_model(self):
+        leaspy = Leaspy('gaussian_distribution')
+        self.assertEqual(leaspy.type, 'gaussian_distribution')
+
+        # Create the data
+        data_path = os.path.join(test_data_dir, 'univariate_data.csv')
+        reader = DataReader()
+        data = reader.read(data_path)
+
+        leaspy.fit(data, os.path.join(test_data_dir, "algorithm_settings.json"))
+        self.assertAlmostEqual(leaspy.model.model_parameters['mu'], 0.1493, delta=0.001)

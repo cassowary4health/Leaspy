8e34d39161f88aa53d38a71163877c841de7cabc
Raphael <raphael.couronne@gmail.com>
2019-03-17 13:00:02 +0100

    [FEAT] Gaussian Model : Initialize Realization

--- src/models/gaussian_distribution_model.py
+++ src/models/gaussian_distribution_model.py
@@ -16,8 +16,16 @@ class GaussianDistributionModel(AbstractModel):
         if reader.model_type != 'gaussian_distribution':
             raise ValueError("The default univariate parameters are not of gaussian_distribution type")
 
-        # TODO to Pytorch
-        self.model_parameters['mu'] = Variable(torch.tensor(self.model_parameters['mu']).float(), requires_grad=True)
+        self.model_parameters = reader.parameters
+
+
+        # TODO to Pytorch, peut être dans le reader ????
+        for key in self.model_parameters.keys():
+            self.model_parameters[key] = Variable(torch.tensor(self.model_parameters[key]).float(), requires_grad=True)
+
+
+    def initialize_individual_realization(self, individual):
+        individual.individual_parameters['a'] = 0
 
     def compute_individual(self, indiviual):
         return self.model_parameters['mu'] + indiviual.individual_parameters['a']*torch.ones_like(indiviual.tensor_timepoints)
@@ -25,7 +33,7 @@ class GaussianDistributionModel(AbstractModel):
     def compute_attachment(self, data):
         likelihood = 0
         for individual in data:
-            likelihood += self.compute_individual_fit_likelihood(individual)
+            likelihood += self.compute_individual_attachment(individual)
         return likelihood
 
     def compute_individual_attachment(self, individual):

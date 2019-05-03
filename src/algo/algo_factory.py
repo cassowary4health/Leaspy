5869e81537deed3066806500b72d937f09dc2664
Raphael <raphael.couronne@gmail.com>
2019-05-03 18:48:07 +0200

    [FIX] imports

--- src/algo/algo_factory.py
+++ src/algo/algo_factory.py
@@ -1,6 +1,5 @@
 from src.algo.gradient_descent import GradientDescent
 from src.algo.greedy_sampling import GreedySampling
-from src.algo.random_sampling import RandomSampling
 from src.algo.mcmc_saem import MCMCSAEM
 
 class AlgoFactory():
@@ -9,9 +8,6 @@ class AlgoFactory():
     def algo(type):
         if type.lower() == 'gradient_descent':
             return GradientDescent()
-        elif type.lower() == 'greedy_sampling':
-            return GreedySampling()
-        elif type.lower() == 'random_sampling':
             return RandomSampling()
         elif type.lower() == 'mcmc_saem':
             return MCMCSAEM()

bb416af82cdf1286a15a314b81f130862af579f2
Raphael <raphael.couronne@gmail.com>
2019-05-04 08:34:20 +0200

    [FIX] output plot+print

--- src/algo/abstract_algo.py
+++ src/algo/abstract_algo.py
@@ -1,3 +1,6 @@
+import torch
+import numpy as np
+
 class AbstractAlgo():
 
 
@@ -7,3 +10,13 @@ class AbstractAlgo():
                 previous_v = self.algo_parameters[k]
                 print("Replacing {} parameter from value {} to value {}".format(k, previous_v, v))
             self.algo_parameters[k] = v
+
+    def _initialize_seed(self, seed):
+        if seed is not None:
+            torch.manual_seed(seed)
+            np.random.seed(seed)
+            print(" ==> Setting seed to {0}".format(seed))
+
+    def _initialize_path_output(self, path_output):
+        if path_output is not None:
+            self.path_output = path_output

b5ce174ae7ea394d77809952bb760965e02892fa
Raphael <raphael.couronne@gmail.com>
2019-06-18 11:02:45 +0200

    [FIX] Random variable test update

--- tests/utils/random_variable/gaussian_random_variable.py
+++ tests/utils/random_variable/gaussian_random_variable.py
@@ -9,7 +9,17 @@ class GaussianRandomVariableTest(unittest.TestCase):
     def test_construction(self):
         mu = 1
         variance = 2
-        gaussian_rv = GaussianRandomVariable(name="test", mu=mu, variance=variance)
+
+        rv_infos = {
+            "name": "tau",
+            "shape": (10, 1),
+            "type": "individual",
+            "rv_type": "gaussian"
+        }
+
+        gaussian_rv = GaussianRandomVariable(rv_infos)
+        gaussian_rv._from_parameters(mu=mu, variance=variance)
+
         self.assertAlmostEqual(gaussian_rv.mu, 1, delta=1e-5)
         self.assertAlmostEqual(gaussian_rv.variance, 2, delta=1e-5)
         self.assertAlmostEqual(gaussian_rv.variance_inverse, 0.5, delta=1e-5)
@@ -18,7 +28,15 @@ class GaussianRandomVariableTest(unittest.TestCase):
     def test_getters_setters_variance(self):
         mu = 1
         variance = 2
-        gaussian_rv = GaussianRandomVariable(name="test", mu=mu, variance=variance)
+        rv_infos = {
+            "name": "tau",
+            "shape": (10, 1),
+            "type": "individual",
+            "rv_type": "gaussian"
+        }
+
+        gaussian_rv = GaussianRandomVariable(rv_infos)
+        gaussian_rv._from_parameters(mu=mu, variance=variance)
         self.assertAlmostEqual(gaussian_rv.log_constant, 1.26551212, delta=1e-5)
 
         gaussian_rv.variance = 1
@@ -29,7 +47,15 @@ class GaussianRandomVariableTest(unittest.TestCase):
     def test_compute_likelihood(self):
         mu = 1
         variance = 2
-        gaussian_rv = GaussianRandomVariable(name="test", mu=mu, variance=variance)
+        rv_infos = {
+            "name": "tau",
+            "shape": (10, 1),
+            "type": "individual",
+            "rv_type": "gaussian"
+        }
+
+        gaussian_rv = GaussianRandomVariable(rv_infos)
+        gaussian_rv._from_parameters(mu=mu, variance=variance)
         self.assertAlmostEqual(gaussian_rv.compute_negativeloglikelihood(torch.Tensor([1])), 1.26551, delta=1e-5)
         self.assertAlmostEqual(gaussian_rv.compute_negativeloglikelihood(torch.Tensor([2])), 1.7655121, delta=1e-5)
         self.assertAlmostEqual(gaussian_rv.compute_negativeloglikelihood(torch.Tensor([3])), 3.265512, delta=1e-5)

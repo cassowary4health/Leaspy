[FIX] Bug in sampler adaptative proposition variance

src/utils/sampler.py
@@ -24,11 +24,11 @@ class Sampler:
         :return:
         """
 
-        accepted = 1
+        accepted = 0
 
         if alpha >= 1:
             # Case 1: we improved the LogL
-            return accepted
+            accepted = 1
 
         else:
             # Case 2: we decreased the LogL
@@ -37,8 +37,8 @@ class Sampler:
             realization = np.random.uniform(low=0, high=1)
 
             # Choose to keep a lesser parameter value from it
-            if realization > alpha:
-                accepted = 0
+            if realization < alpha:
+                accepted = 1
 
         # Update acceptance rate of sampler
         self.update_acceptation_rate(accepted)

tests/utils/sampler.py
@@ -35,7 +35,7 @@ class SamplerTest(unittest.TestCase):
         accepted_list = np.array(accepted_list)
         self.assertAlmostEqual(np.mean(accepted_list), 0.5, delta=0.05)
 
-    """
+
     #TODO why does noit it work ?
     def test_adaptative_proposition_variance(self):
 
@@ -64,7 +64,7 @@ class SamplerTest(unittest.TestCase):
             if accepted:
                 real = prop_real
 
-        self.assertAlmostEqual(np.mean(accepted_list[-200:]), 0.3, delta=0.2)"""
+        self.assertAlmostEqual(np.mean(accepted_list[-200:]), 0.3, delta=0.2)
 
 
 
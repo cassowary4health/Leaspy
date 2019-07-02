a83bf11280bace177403724a29420c1dd62f2c69
Raphael <raphael.couronne@gmail.com>
2019-07-02 14:33:26 +0200

    [FIX] Value error quickstart

--- example/quickstart.py
+++ example/quickstart.py
@@ -1,49 +1,9 @@
 import os
-
-"""
-import pandas as pd
-data_path = os.path.join("../data_leaspy/", "data_adas",'df.csv')
-df = pd.read_csv(data_path).iloc[:,[0,1,6]]
-df.columns = ['ID','AGE','Value']
-df.set_index(['ID','AGE'], inplace=True)
-data_dir = "../data_leaspy/quickstart"
-
-
-import numpy as np
-age_mean = np.mean(df.index.get_level_values(level=1))
-age_std = np.std(df.index.get_level_values(level=1))
-
-df.reset_index(inplace=True)
-df['AGE']= (df['AGE']-age_mean)/age_std
-df.set_index(['ID', 'AGE'])
-
-df.to_csv(os.path.join(data_dir, 'df_univariate.csv'))
-
-
-import pandas as pd
-data_dir = "example/multivariate/"
-data_path = os.path.join(data_dir, 'data.csv')
-
-df = pd.read_csv(data_path)
-df.set_index(['ID','TIME'], inplace=True)
-df = df.loc[df.index.drop_duplicates(keep=False)]
-df.to_csv(os.path.join(data_dir, 'data2.csv'))
-
-"""
-
-
-#%%
-
-
-#%%
-
-import os
 from src.main import Leaspy
 from src.inputs.data_reader import DataReader
 from src.inputs.algo_settings import AlgoSettings
 
 # Settings
-
 project_dir = os.path.join(os.path.dirname(__file__), '..')
 
 
@@ -57,7 +17,11 @@ algo_settings = AlgoSettings(path_to_algo_parameters)
 # Output folder
 path_output = os.path.join(project_dir, '../output_leaspy/quickstart/')
 
+if not os.path.exists(path_output):
+    raise ValueError("output folder {0} does not exists yet".format(path_output))
+
 algo_settings.output_path = path_output
+
 
 # Leaspy instanciate
 #leaspy = Leaspy.from_model_settings(path_to_model_parameters)
@@ -65,6 +29,7 @@ leaspy = Leaspy('multivariate')
 
 # Load the data
 data_path = os.path.join(data_dir, 'data2_tiny.csv')
+#data_path = os.path.join(data_dir, 'data2.csv')
 reader = DataReader()
 data = reader.read(data_path)
 

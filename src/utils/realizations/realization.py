[FIX] Initialization with low variance at beginning

src/utils/realizations/realization.py
@@ -18,7 +18,7 @@ class Realization:
         elif self.variable_type == 'individual':
 
             distribution = torch.distributions.normal.Normal(loc=model.parameters["{0}_mean".format(self.name)],
-                                                             scale=model.parameters["{0}_std".format(self.name)])
+                                                             scale=0.01* model.parameters["{0}_std".format(self.name)]) # TODO change later, to have low variance when initialized
             self._tensor_realizations = distribution.sample(sample_shape = (data.n_individuals, self.shape[0], self.shape[1]))
 
             self.is_autograd = False
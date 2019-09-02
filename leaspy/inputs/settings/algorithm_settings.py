6009ddd1c99513ea8fcd182a7c34ef16cfee026a
Symcies <koval.igor.92@gmail.com>
2019-09-02 11:55:13 +0200

    [FIX] Default number of burn-in

--- leaspy/inputs/settings/algorithm_settings.py
+++ leaspy/inputs/settings/algorithm_settings.py
@@ -30,8 +30,9 @@ class AlgorithmSettings:
 
         # Todo : Make it better
         if name == 'mcmc_saem':
-            if 'n_iter' in kwargs.keys() and 'n_burn_in_iter' not in kwargs.keys():
+            if 'n_iter' in kwargs.keys() and 'n_burn_in_iter' is None:
                 self.parameters['n_burn_in_iter'] = int(0.9 * kwargs['n_iter'])
+                
             if 'n_iter' in kwargs.keys() and 'annealing' not in kwargs.keys():
                 self.parameters['annealing']["n_iter"] = int(0.5 * kwargs['n_iter'])
 

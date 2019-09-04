7225d3099897ca1976e65e96c921fafa23b0177d
Raphael <raphael.couronne@gmail.com>
2019-09-04 17:42:42 +0200

    [FEAT] patient directory from result

--- leaspy/inputs/data/result.py
+++ leaspy/inputs/data/result.py
@@ -31,10 +31,18 @@ class Result:
         return distributions
 
 
-
-
-
-
     def get_cofactor_distribution(self, cofactor):
         return [d.cofactors[cofactor] for d in self.data]
+
+    def get_patient_individual_parameters(self, idx):
+
+        indices = list(self.data.individuals.keys())
+        idx_number = int(np.where(np.array(indices) == idx)[0]) # TODO save somewhere the correspondance : in data probably ???
+
+        patient_dict = dict.fromkeys(self.individual_parameters.keys())
+
+        for variable_ind in list(self.individual_parameters.keys()):
+            patient_dict[variable_ind] = self.individual_parameters[variable_ind][idx_number]
+
+        return patient_dict
 

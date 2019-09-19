d1bf77d7a9c646490bc73864c61d47a8cb08bab9
Arnaud_Valladier <arnaud.valladier@polytechnique.edu>
2019-09-19 17:19:58 +0200

    Unit test for initialization of multivariate parallel model class

--- tests/unit_tests/models/logistic_parallel_model.py
+++ tests/unit_tests/models/logistic_parallel_model.py
@@ -1,7 +1,21 @@
-import unittest
+from .abstract_multivariate_model import AbstractMultivariateModelTest
+
+from leaspy.models.multivariate_parallel_model import MultivariateParallelModel
 
 
-class Test(unittest.TestCase):
+class MultivariateParallelModelTest(AbstractMultivariateModelTest):
 
-    def test_constructor(self):
-        return 0
+    def test_constructor_multivariate_parallel(self, model=None):
+        """
+        Test attribute's initialization of leaspy multivariate parallel model
+        :param model: leaspy model object
+        :return: exit code
+        """
+        print("Unit-test constructor MultivariateModel")
+
+        if model is None:
+            model = MultivariateParallelModel("logistic")
+            self.assertEqual(type(model), MultivariateParallelModel)
+
+        self.assertEqual(model.parameters['deltas'], None)
+        self.assertEqual(model.MCMC_toolbox['priors']['deltas_std'], None)

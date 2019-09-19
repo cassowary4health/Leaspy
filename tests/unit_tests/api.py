a45cdf8ebb04096af0c5070d0ca4ae5fa04f75ca
Arnaud_Valladier <arnaud.valladier@polytechnique.edu>
2019-09-19 18:05:07 +0200

    No more tests in unit_tests/api.py + refinement of unit_tests/models/model_factory.py

--- tests/unit_tests/api.py
+++ tests/unit_tests/api.py
@@ -1,22 +1,22 @@
-import unittest
+# import unittest
+#
+# from leaspy.api import Leaspy
+# from .models.univariate_model import UnivariateModelTest
+#
 
-from leaspy.api import Leaspy
-from .models.univariate_model import UnivariateModelTest
-
-
-class LeaspyTest(unittest.TestCase):
-
-    def test_constructor_univariate(self, leaspy=None):
-        """
-        Test attribute's initialization of leaspy univariate model
-        :param leaspy: leaspy class object - used by test_constructor_multivariate function for common attributes
-        :return: exit code
-        """
-        if leaspy is None:
-            # Univariate model
-            leaspy = Leaspy('univariate')
-            self.assertEqual(leaspy.type, 'univariate')
-        UnivariateModelTest().test_univariate_constructor(leaspy.model)
+# class LeaspyTest(unittest.TestCase):
+#
+#     def test_constructor_univariate(self, leaspy=None):
+#         """
+#         Test attribute's initialization of leaspy univariate model
+#         :param leaspy: leaspy class object - used by test_constructor_multivariate function for common attributes
+#         :return: exit code
+#         """
+#         if leaspy is None:
+#             # Univariate model
+#             leaspy = Leaspy('univariate')
+#             self.assertEqual(leaspy.type, 'univariate')
+#         UnivariateModelTest().test_univariate_constructor(leaspy.model)
 
 
 #TODO
--- tests/unit_tests/models/model_factory.py
+++ tests/unit_tests/models/model_factory.py
@@ -3,25 +3,43 @@ import unittest
 from leaspy.models.model_factory import ModelFactory
 from leaspy.models.univariate_model import UnivariateModel
 
+from .univariate_model import UnivariateModelTest
+from .logistic_model import MultivariateModelTest
+from .logistic_parallel_model import MultivariateParallelModelTest
+
 
 class ModelFactoryTest(unittest.TestCase):
 
-    def test_model(self):
-        """Test model static method"""
-        # Test lower case
+    def test_model_factory_constructor(self, name=None):
+        """
+        Test attribute's initialization of leaspy univariate model
+        :param name: string - name of the model
+        :return: exit code
+        """
+        print("Unit-test model factory constructor")
+        if name is None:
+            for name in ['univariate', 'linear', 'logistic', 'logistic_parallel']:
+                self.test_model_factory_constructor(name)
+        else:
+            model = ModelFactory().model(name)
+
+            if name == 'univariate':
+                UnivariateModelTest().test_univariate_constructor(model)
+            elif name == 'logistic' or name == 'linear':
+                MultivariateModelTest().test_constructor_multivariate(model)
+            elif name == 'logistic_parallel':
+                MultivariateParallelModelTest().test_constructor_multivariate_parallel(model)
+
+    def test_lower_case(self):
+        """Test lower case"""
         name_exemples = ['univariate', 'uNIVariaTE', 'UNIVARIATE']
         for name in name_exemples:
             model = ModelFactory.model(name)
             # Test model type
             self.assertEqual(type(model), UnivariateModel)
 
-        # Test initialization of UnivariateModel
-        self.assertEqual(model.parameters['g'], None)
-        self.assertEqual(model.parameters['tau_mean'], None)
-        self.assertEqual(model.parameters['tau_std'], None)
-        self.assertEqual(model.parameters['xi_mean'], None)
-        self.assertEqual(model.parameters['xi_std'], None)
-
+    def test_wrong_arg(self):
+        """Test if raise error for wrong argument"""
         # Test if raise ValueError if wrong string arg for name
         wrong_arg_exemples = ['lgistic', 'blabla']
         for wrong_arg in wrong_arg_exemples:

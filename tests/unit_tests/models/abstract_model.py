9bb403ac48e8ac6e9e859667bad987fca272329f
Arnaud_Valladier <arnaud.valladier@polytechnique.edu>
2019-09-19 17:16:09 +0200

    Unit test for initialization of abstract model class

--- tests/unit_tests/models/abstract_model.py
+++ tests/unit_tests/models/abstract_model.py
@@ -5,12 +5,29 @@ from leaspy.models.abstract_model import AbstractModel
 
 class AbstractModelTest(unittest.TestCase):
 
+    def test_abstract_model_constructor(self):
+        """
+        Test initialization of abstract model class object
+        :return: exit code
+        """
+        print("Unit-test AbstractModel")
+
+        model = AbstractModel("dummy_abstractmodel")
+        self.assertEqual(model.parameters, None)
+
+        # Test the presence of all these essential methods
+        main_methods = ['load_parameters', 'get_individual_variable_name', 'compute_sum_squared_tensorized',
+                        'compute_individual_attachment_tensorized_mcmc', 'compute_individual_attachment_tensorized',
+                        'update_model_parameters', 'update_model_parameters_burn_in',
+                        'get_population_realization_names', 'get_individual_realization_names',
+                        'compute_regularity_realization', 'compute_regularity_variable', 'get_realization_object']
+
+        present_attributes = [_ for _ in dir(model) if _[:2] != '__']  # Get the present method
+
+        for attribute in main_methods:
+            self.assertTrue(attribute in present_attributes)
+
     #TODO
-
-    def test_constructor(self):
-        abstract_model = AbstractModel("dummy_abstractmodel")
-        self.assertEqual(abstract_model.parameters, None)
-
     """
     def test_load_parameters(self):
         abstract_model = AbstractModel()

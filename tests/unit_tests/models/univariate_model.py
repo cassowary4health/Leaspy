ce348e11574c4d9ffeb9a96959e70ea8626a5b67
Arnaud_Valladier <arnaud.valladier@polytechnique.edu>
2019-09-19 17:16:57 +0200

    Unit test for initialization of univariate model class

--- tests/unit_tests/models/univariate_model.py
+++ tests/unit_tests/models/univariate_model.py
@@ -1,14 +1,32 @@
-import unittest
-
 from leaspy.models.univariate_model import UnivariateModel
 
+from .abstract_model import AbstractModelTest
 
-class UnivariateModelTest(unittest.TestCase):
 
-    def test_constructor(self):
-        model = UnivariateModel()
-        self.assertEqual(model.model_parameters['p0'], None)
-        self.assertEqual(model.model_parameters['tau_mean'], None)
-        self.assertEqual(model.model_parameters['tau_var'], None)
-        self.assertEqual(model.model_parameters['xi_mean'], None)
-        self.assertEqual(model.model_parameters['xi_var'], None)
+class UnivariateModelTest(AbstractModelTest):
+
+    def test_univariate_constructor(self, model=None):
+        """
+        Test attribute's initialization of leaspy univariate model
+        :return: exit code
+        """
+        print('Unit-test UnivariateModel')
+
+        if model is None:
+            model = UnivariateModel('univariate')
+            self.assertEqual(type(model), UnivariateModel)
+            self.assertEqual(model.dimension, 1)
+            self.assertEqual(model.source_dimension, 0)
+
+        self.assertEqual(model.attributes, None)
+        self.assertEqual(model.bayesian_priors, None)
+
+        self.assertEqual(model.parameters['g'], None)
+        self.assertEqual(model.parameters['noise_std'], None)
+        self.assertEqual(model.parameters['tau_mean'], None)
+        self.assertEqual(model.parameters['tau_std'], None)
+        self.assertEqual(model.parameters['xi_mean'], None)
+        self.assertEqual(model.parameters['xi_std'], None)
+
+        self.assertEqual(model.MCMC_toolbox['attributes'], None)
+        self.assertEqual(model.MCMC_toolbox['priors']['g_std'], None)

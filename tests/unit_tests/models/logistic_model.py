5c422ae0d32d11040e1f9f6ac549f5f01f9cbc9c
Arnaud_Valladier <arnaud.valladier@polytechnique.edu>
2019-09-19 17:19:15 +0200

    Unit test for initialization of multivariate model class

--- tests/unit_tests/models/logistic_model.py
+++ tests/unit_tests/models/logistic_model.py
@@ -1,7 +1,21 @@
-import unittest
+from .abstract_multivariate_model import AbstractMultivariateModelTest
+
+from leaspy.models.multivariate_model import MultivariateModel
 
 
-class Test(unittest.TestCase):
+class MultivariateModelTest(AbstractMultivariateModelTest):
 
-    def test_constructor(self):
-        return 0
+    def test_constructor_multivariate(self, model=None):
+        """
+        Test attribute's initialization of leaspy multivariate model
+        :param model: leaspy model object
+        :return: exit code
+        """
+        print("Unit-test constructor MultivariateModel")
+
+        if model is None:
+            model = MultivariateModel("logistic")
+            self.assertEqual(type(model), MultivariateModel)
+
+        self.assertEqual(model.parameters['v0'], None)
+        self.assertEqual(model.MCMC_toolbox['priors']['v0_std'], None)

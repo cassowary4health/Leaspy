bebc2036d48ea8bb7cf31a716fad7e17d1c547ba
Arnaud_Valladier <arnaud.valladier@polytechnique.edu>
2019-10-11 17:14:40 +0200

    [FIX] dataset.py - remove the use of numpy (+bug fix in data.py)

--- leaspy/inputs/data/data.py
+++ leaspy/inputs/data/data.py
@@ -85,7 +85,7 @@ class Data:
             for j, age in enumerate(ages):
                 indices.append(indiv.idx)
                 timepoints[iteration] = age
-                arr[iteration] = indiv.observations[j]
+                arr[iteration] = torch.tensor(indiv.observations[j])
 
                 iteration += 1
 

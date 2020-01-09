8a49a8c7c1547c290d9cb5b14a657839724d9651
Arnaud_Valladier <arnaud.valladier@polytechnique.edu>
2020-01-09 18:35:45 +0100

    [FIX] Fix leaspy_parallel.py parallel_backend for joblib (+ pep8 & docstrings)

--- leaspy/utils/parallel/leaspy_parallel.py
+++ leaspy/utils/parallel/leaspy_parallel.py
@@ -1,26 +1,34 @@
-from joblib import Parallel, delayed
+from joblib import Parallel, delayed, parallel_backend
 
-def leaspy_parallel_calibrate(data_iter, algo_settings_iter, leaspy_factory, leaspy_obj_cb, n_jobs=-1, **joblib_Parallel_kwargs):
+
+def leaspy_parallel_calibrate(data_iter, algo_settings_iter, leaspy_factory, leaspy_obj_cb,
+                              n_jobs=-1, **joblib_Parallel_kwargs):
     """
-    Calibrate in parallel multiple Leaspy models
+    Calibrate in parallel multiple Leaspy models.
 
     Parameters
     ----------
-    data_iter: an iterable of Leaspy Data objects to be calibrated on
-    algo_settings_iter: an iterable of Leaspy AlgorithmSettings for every calibration task
+    data_iter : `list` [`Data`]
+        An iterable of Leaspy Data objects to be calibrated on.
+    algo_settings_iter : `list` [`AlgorithmSettings`]
+        An iterable of Leaspy AlgorithmSettings for every calibration task.
+    leaspy_factory : callable
+        A function taking as input iteration index and returning a new Leaspy object that will be calibrated.
+    leaspy_obj_cb : callable
+        A function taking as input a calibrated Leaspy object and iteration index and doing whatsoever needed with it
+        (i.e.: saving model to a file, ...).
+    n_jobs : `int` (default -1)
+        The number of parallel jobs in joblib.
+    **joblib_Parallel_kwargs
+        Other joblib Parallel parameters (such as `verbose`, ...).
 
-    leaspy_factory: a function taking as input iteration index and returning a new Leaspy object that will be calibrated
-    leaspy_obj_cb: a function taking as input a calibrated Leaspy object and iteration index and doing whatsoever needed with it (i.e.: saving model to a file, ...)
-
-    n_jobs: number of parallel jobs in joblib (default to -1)
-    **joblib_Parallel_kwargs: other joblib Parallel parameters (such as `verbose`, ...)
-
-    Output
-    ----------
-    list of `leaspy_obj_cb` outputs for every jobs
+    Returns
+    -------
+    result : `list`
+        Contains the `leaspy_obj_cb` outputs for every jobs.
     """
-
     # unitary job
+    @delayed
     def calibrate_job(data, algo_settings, i):
         # create Leaspy object for this job from the prescribed factory
         leaspy = leaspy_factory(i)
@@ -29,36 +37,47 @@ def leaspy_parallel_calibrate(data_iter, algo_settings_iter, leaspy_factory, lea
         # do something with the calibrated model
         return leaspy_obj_cb(leaspy, i)
 
-    return Parallel(n_jobs=n_jobs, **joblib_Parallel_kwargs)(delayed(calibrate_job)(data, algo_settings, i) \
-                                                             for i, (data, algo_settings) in enumerate(zip(data_iter, algo_settings_iter)))
+    with parallel_backend('threading'):  # seems not to work for other backends
+        result = Parallel(n_jobs=n_jobs, **joblib_Parallel_kwargs)(calibrate_job(data, algo_settings, i)
+                                                                   for i, (data, algo_settings)
+                                                                   in enumerate(zip(data_iter, algo_settings_iter)))
+    return result
 
-def leaspy_parallel_personalize(leaspy_iter, data_iter, algo_settings_iter, leaspy_res_cb, n_jobs=-1, **joblib_Parallel_kwargs):
+
+def leaspy_parallel_personalize(leaspy_iter, data_iter, algo_settings_iter, leaspy_res_cb,
+                                n_jobs=-1, **joblib_Parallel_kwargs):
     """
     Personalize in parallel multiple Leaspy models
 
     Parameters
     ----------
-    leaspy_iter: an iterable of Leaspy objects to personalize on
-    data_iter: an iterable of Leaspy Data objects to be personalized
-    algo_settings_iter: an iterable of Leaspy AlgorithmSettings for every personalization task
+    leaspy_iter : `list` [`Leaspy`]
+        An iterable of Leaspy objects to personalize on
+    data_iter : `list` [`Data`]
+        An iterable of Leaspy Data objects to be calibrated on.
+    algo_settings_iter : `list` [`AlgorithmSettings`]
+        An iterable of Leaspy AlgorithmSettings for every calibration task.
+    leaspy_res_cb : callable
+        A function taking as input a Leaspy Result object (the output of personalization task) and iteration index
+        and doing whatsoever needed with it (i.e.: saving individual parameters/results to a file, ...).
+    n_jobs : `int` (default -1)
+        The number of parallel jobs in joblib.
+    **joblib_Parallel_kwargs
+        Other joblib Parallel parameters (such as `verbose`, ...).
 
-    leaspy_res_cb: a function taking as input a Leaspy Result object (the output of personalization task) and iteration index
-                   and doing whatsoever needed with it (i.e.: saving individual parameters/results to a file, ...)
-
-    n_jobs: number of parallel jobs in joblib (default to -1)
-    **joblib_Parallel_kwargs: other joblib Parallel parameters (such as `verbose`, ...)
-
-    Output
-    ----------
-    list of `leaspy_res_cb` outputs for every jobs
+    Returns
+    -------
+    `list`
+        Contains the `leaspy_res_cb` outputs for every jobs.
     """
-
     # unitary job
+    @delayed
     def personalize_job(leaspy, data, algo_settings, i):
         # personalize calibrated model with prescribed data and settings
         r = leaspy.personalize(data, algo_settings)
         # do something with results of personalization
         return leaspy_res_cb(r, i)
 
-    return Parallel(n_jobs=n_jobs, **joblib_Parallel_kwargs)(delayed(personalize_job)(leaspy, data, algo_settings, i) \
-                                                             for i, (leaspy, data, algo_settings) in enumerate(zip(leaspy_iter, data_iter, algo_settings_iter)))
+    return Parallel(n_jobs=n_jobs, **joblib_Parallel_kwargs)(
+        personalize_job(leaspy, data, algo_settings, i) for i, (leaspy, data, algo_settings)
+        in enumerate(zip(leaspy_iter, data_iter, algo_settings_iter)))
--- tests/unit_tests/utils/parallel/test_leaspy_parallel.py
+++ tests/unit_tests/utils/parallel/test_leaspy_parallel.py
@@ -1,11 +1,11 @@
 import unittest
 
-from leaspy import Leaspy, Data, AlgorithmSettings
-from leaspy.inputs.data.result import Result
+import numpy as np
 
+from leaspy import AlgorithmSettings, Data, Leaspy
+from leaspy.inputs.data.result import Result
 from leaspy.utils.parallel import leaspy_parallel_calibrate, leaspy_parallel_personalize
 
-import numpy as np
 
 class TestLeaspyParallel(unittest.TestCase):
 
@@ -14,52 +14,51 @@ class TestLeaspyParallel(unittest.TestCase):
 
         self.n_runs = 9
 
-        self.model_type = 'logistic'
-        #self.src_dim = 1
-
         seed = 42
         np.random.seed(seed)
 
-        self.fit_algos_settings = [AlgorithmSettings('mcmc_saem', n_iter=10, seed=seed) for r in range(self.n_runs)]
-        self.perso_algos_settings = [AlgorithmSettings('mode_real', seed=seed) for r in range(self.n_runs)]
-
-        p = 5 # pats number
-        v = 3 # vis number
-        f = 2 # feat number
+        p = 5  # pats number
+        v = 3  # vis number
+        f = 2  # feat number
         t = np.sort(np.random.rand(self.n_runs, p, v), axis=2)
         y = np.sort(np.random.rand(self.n_runs, p, v, f), axis=2)
 
-        self.datas = [Data.from_individuals(np.arange(p),t[r,:],y[r,:],[f'feature_{i}' for i in range(f)]) for r in range(self.n_runs)]
+        self.datas = [Data.from_individuals(np.arange(p), t[r, :], y[r, :],
+                                            [f'feature_{i}' for i in range(f)]) for r in range(self.n_runs)]
+        self.model_type = 'logistic'
+        self.settings_algos_fit = [AlgorithmSettings('mcmc_saem', n_iter=10, seed=seed)] * self.n_runs
+        self.settings_algos_perso = [AlgorithmSettings('mode_real', seed=seed)] * self.n_runs
+        # self.src_dim = 1
 
     def test_fit_and_perso(self):
 
-        def leaspy_factory(i):
+        def leaspy_factory(_):
             leaspy = Leaspy(self.model_type)
-            #leaspy.model.load_hyperparameters({'source_dimension': self.src_dim}) # Optional
+            # leaspy.model.load_hyperparameters({'source_dimension': self.src_dim}) # Optional
             return leaspy
 
-        def leaspy_cb(leaspy, i):
-            return (leaspy, i)
+        def leaspy_cb(leaspy, k):
+            return leaspy, k
 
-        def leaspy_res_cb(res, i):
-            return (res, i)
+        def leaspy_res_cb(res, k):
+            return res, k
 
-        outs_fit_cb = leaspy_parallel_calibrate(self.datas, self.fit_algos_settings, leaspy_factory, leaspy_cb, verbose=50)
+        outs_fit_cb = leaspy_parallel_calibrate(self.datas, self.settings_algos_fit,
+                                                leaspy_factory, leaspy_cb, verbose=50)
 
         self.assertEqual(len(outs_fit_cb), self.n_runs)
         for i, (leaspy, j) in enumerate(outs_fit_cb):
             with self.subTest(i=i):
-                self.assertEqual(i,j) # order outputs
-                self.assertIsInstance(leaspy, Leaspy) # leaspy object
-                leaspy.check_if_initialized() # raises an error otherwise
+                self.assertEqual(i, j)  # order outputs
+                self.assertIsInstance(leaspy, Leaspy)  # leaspy object
+                leaspy.check_if_initialized()  # raises an error otherwise
 
-        leaspys = [out[0] for out in outs_fit_cb] # iterable of leaspy fitted models
-        outs_perso_cb = leaspy_parallel_personalize(leaspys, self.datas, self.perso_algos_settings, leaspy_res_cb, verbose=50)
+        leaspys = [out[0] for out in outs_fit_cb]  # iterable of leaspy fitted models
+        outs_perso_cb = leaspy_parallel_personalize(leaspys, self.datas, self.settings_algos_perso,
+                                                    leaspy_res_cb, verbose=50)
 
         self.assertEqual(len(outs_perso_cb), self.n_runs)
         for i, (res, j) in enumerate(outs_perso_cb):
             with self.subTest(i=i):
-                self.assertEqual(i,j) # order outputs
-                self.assertIsInstance(res, Result) # leaspy object
-
-        return 0
+                self.assertEqual(i, j)  # order outputs
+                self.assertIsInstance(res, Result)  # leaspy object

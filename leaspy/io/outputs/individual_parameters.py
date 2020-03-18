c31eb3fc0ae7363d68748826ef945d2ec9c59264
Symcies <koval.igor.92@gmail.com>
2020-03-18 23:05:06 +0100

    [FEAT] Individual parameters : from/to conversion for dataframe, pytorch and dict initiated + related tests

--- leaspy/io/outputs/individual_parameters.py
+++ leaspy/io/outputs/individual_parameters.py
@@ -1,6 +1,7 @@
 import torch
 import pandas as pd
 import numpy as np
+import warnings
 
 class IndividualParameters:
     """
@@ -18,35 +19,36 @@ class IndividualParameters:
     """
 
     def __init__(self):
-        self.indices = []
-        self.individual_parameters = {}
-        self.parameters_shape = {} # {p_name: p_shape}
+        self._indices = []
+        self._individual_parameters = {}
+        self._parameters_shape = {} # {p_name: p_shape}
+        self._default_saving_type = 'csv'
 
     def add_individual_parameters(self, index, individual_parameters):
-        if index in self.indices:
+        if index in self._indices:
             raise ValueError(f'The index {index} has already been added before')
-        self.indices.append(index)
-        self.individual_parameters[index] = individual_parameters
+        self._indices.append(index)
+        self._individual_parameters[index] = individual_parameters
 
         for k, v in individual_parameters.items():
             ### Keep track of the parameter shape
-            if k not in self.parameters_shape.keys():
-                self.parameters_shape[k] = 1 if np.ndim(v) == 0 else len(v)
+            if k not in self._parameters_shape.keys():
+                self._parameters_shape[k] = 1 if np.ndim(v) == 0 else len(v)
 
     def to_dataframe(self):
         """
 
         """
-        p_names = list(self.parameters_shape.keys())
+        p_names = list(self._parameters_shape.keys())
 
         # Get the data, idx per idx
         arr = []
-        for idx in self.indices:
+        for idx in self._indices:
             indiv_arr = [idx]
-            indiv_p = self.individual_parameters[idx]
+            indiv_p = self._individual_parameters[idx]
 
             for p_name in p_names:
-                shape = self.parameters_shape[p_name]
+                shape = self._parameters_shape[p_name]
 
                 if shape == 1:
                     indiv_arr.append(indiv_p[p_name])
@@ -58,40 +60,74 @@ class IndividualParameters:
         # Get the column names
         final_names = ['ID']
         for p_name in p_names:
-            shape = self.parameters_shape[p_name]
+            shape = self._parameters_shape[p_name]
             if shape == 1:
                 final_names.append(p_name)
             else:
                 final_names += [p_name+'_'+str(i) for i in range(shape)]
 
-
         df = pd.DataFrame(arr, columns=final_names)
         return df.set_index('ID')
 
 
-
-    def from_dataframe(self):
+    @staticmethod
+    def from_dataframe(df):
         """
 
         """
+        # Check the names to keep
+        df_names = list(df.columns.values)
 
-    def from_pytorch(self):
+        final_names = {}
+        for name in df_names:
+            split = name.split('_')[0]
+            if split not in final_names:
+                final_names[split] = []
+            final_names[split].append(name)
+
+        final_names = {k: v if len(v) > 1 else v[0] for k, v in final_names.items()}
+
+        # Create the individual parameters
+        ip = IndividualParameters()
+
+        for idx, v_flat in df.iterrows():
+            i_d = {k: v_flat[v] if np.ndim(v) == 0 else v_flat[v].values.tolist() for k, v in final_names.items()}
+            ip.add_individual_parameters(idx, i_d)
+
+        return ip
+
+    @staticmethod
+    def from_pytorch(dict_pytorch):
         """
 
         """
+        warnings.warn("The `from_pytorch` method of IndividualParameters object"
+                      "is unaware of the subject indices. They are initialized with default values")
+        ip = IndividualParameters()
+
+        keys = list(dict_pytorch.keys())
+        n_subjects = len(dict_pytorch[keys[0]])
+
+        for idx in range(n_subjects):
+            p = {k: dict_pytorch[k][idx].numpy().tolist() for k in keys}
+            p = {k: v[0] if len(v) == 1 else v for k, v in p.items()}
+
+            ip.add_individual_parameters(idx, p)
+
+        return ip
 
     def to_pytorch(self):
         """
 
         """
         ips_pytorch = {}
-        p_names = list(self.parameters_shape)
+        p_names = list(self._parameters_shape)
 
         for p_name in p_names:
 
-            p_val = [self.individual_parameters[idx][p_name] for idx in self.indices]
+            p_val = [self._individual_parameters[idx][p_name] for idx in self._indices]
             p_val = torch.tensor(p_val, dtype=torch.float32)
-            p_val = p_val.reshape(shape=(len(self.indices), self.parameters_shape[p_name]))
+            p_val = p_val.reshape(shape=(len(self._indices), self._parameters_shape[p_name]))
 
             ips_pytorch[p_name] = p_val
 
@@ -99,15 +135,54 @@ class IndividualParameters:
 
 
     def save_individual_parameters(self, path, extension='.csv'):
-        if path == '.csv':
-            save_csv()
-        elif path == '.json':
-            save_json()
+        extension = IndividualParameters._check_and_get_extension(path)
+        if not extension:
+            warnings.warn(f'You did not provide a valid extension (csv or json) for the file. '
+                          f'Default to {self._default_saving_type}')
+            extension = self._default_saving_type
+
+        if extension == '.csv':
+            IndividualParameters._save_csv(path)
+        elif extension == '.json':
+            IndividualParameters._save_json(path)
+        else:
+            raise ValueError("Something bad happened")
 
     @staticmethod
     def load_individual_parameters(path, verbose=True, **args):
-        if path == '.csv':
-            load_csv()
-        elif path == '.json':
-            load_json()
+        extension = IndividualParameters._check_and_get_extension(path)
+        if not extension or extension not in ['csv', 'json']:
+            raise ValueError('The file you provide should have a `.csv` or `.json` name')
 
+        if extension == 'csv':
+            ip = IndividualParameters._load_csv(path)
+        elif path == 'json':
+            ip = IndividualParameters._load_json(path)
+        else:
+            raise ValueError("Something bad happened")
+
+        return ip
+
+    @staticmethod
+    def _check_and_get_extension(path):
+
+        if '.' in path:
+            extension = path.split('.')[-1]
+            return extension
+        return False
+
+    @staticmethod
+    def _save_csv(path):
+        return 0
+
+    @staticmethod
+    def _save_json(path):
+        return 0
+
+    @staticmethod
+    def _load_csv(path):
+        return 0
+
+    @staticmethod
+    def _load_json(path):
+        return 0
--- tests/unit_tests/io/outputs/test_individual_parameters.py
+++ tests/unit_tests/io/outputs/test_individual_parameters.py
@@ -1,5 +1,7 @@
 import unittest
 import pandas as pd
+import torch
+import numpy as np
 
 from leaspy.io.outputs.individual_parameters import IndividualParameters
 
@@ -8,9 +10,10 @@ class IndividualParametersTest(unittest.TestCase):
     def test_constructor(self):
 
         ip = IndividualParameters()
-        self.assertEqual(ip.indices, [])
-        self.assertEqual(ip.individual_parameters, {})
-        self.assertEqual(ip.parameters_shape, {})
+        self.assertEqual(ip._indices, [])
+        self.assertEqual(ip._individual_parameters, {})
+        self.assertEqual(ip._parameters_shape, {})
+        self.assertEqual(ip._default_saving_type, "csv")
 
     def test_individual_parameters(self):
 
@@ -23,11 +26,11 @@ class IndividualParametersTest(unittest.TestCase):
         ip.add_individual_parameters("idx2", p2)
         ip.add_individual_parameters("idx3", p3)
 
-        self.assertEqual(ip.indices, ["idx1", "idx2", "idx3"])
-        self.assertEqual(ip.individual_parameters, {"idx1": p1, "idx2": p2, "idx3": p3})
-        self.assertEqual(ip.parameters_shape, {"xi": 1, "tau": 1, "sources": 2})
+        self.assertEqual(ip._indices, ["idx1", "idx2", "idx3"])
+        self.assertEqual(ip._individual_parameters, {"idx1": p1, "idx2": p2, "idx3": p3})
+        self.assertEqual(ip._parameters_shape, {"xi": 1, "tau": 1, "sources": 2})
 
-    def test_dataframe(self):
+    def test_to_dataframe(self):
         ip = IndividualParameters()
 
         p1 = {"xi": 0.1, "tau": 70, "sources": [0.1, -0.3]}
@@ -45,3 +48,136 @@ class IndividualParametersTest(unittest.TestCase):
         self.assertTrue((df.index == df_test.index).all())
         for n1, n2 in zip(df.columns, df_test.columns):
             self.assertEqual(n1, n2)
+
+
+    def test_from_dataframe(self):
+
+        p1 = {"xi": 0.1, "tau": 70, "sources": [0.1, -0.3]}
+        p2 = {"xi": 0.2, "tau": 73, "sources": [-0.4, 0.1]}
+
+        df = pd.DataFrame(data=[[0.1, 70, 0.1, -0.3], [0.2, 73, -0.4, 0.1]],
+                          index=["idx1", "idx2"],
+                          columns=["xi", "tau", "sources_0", "sources_1"])
+
+        ip = IndividualParameters.from_dataframe(df)
+
+        self.assertEqual(ip._indices, ["idx1", "idx2"])
+        self.assertEqual(ip._individual_parameters, {"idx1": p1, "idx2": p2})
+        self.assertEqual(ip._parameters_shape, {"xi": 1, "tau": 1, "sources": 2})
+
+    def test_to_from_dataframe(self):
+        df1 = pd.DataFrame(data=[[0.1, 70, 0.1, -0.3], [0.2, 73, -0.4, 0.1]],
+                           index=["idx1", "idx2"],
+                           columns=["xi", "tau", "sources_0", "sources_1"])
+
+        ip1 = IndividualParameters.from_dataframe(df1)
+        df2 = ip1.to_dataframe()
+        ip2 = IndividualParameters.from_dataframe(df2)
+
+        # Test between individual parameters
+        self.assertEqual(ip1._indices, ip2._indices)
+        self.assertDictEqual(ip1._individual_parameters, ip2._individual_parameters)
+        self.assertDictEqual(ip1._parameters_shape, ip2._parameters_shape)
+
+        # Test between dataframes
+        self.assertTrue((df1.values == df2.values).all())
+        self.assertTrue((df1.index == df2.index).all())
+        for n1, n2 in zip(df1.columns, df2.columns):
+            self.assertEqual(n1, n2)
+
+    def test_to_pytorch(self):
+        """
+
+        """
+        ip = IndividualParameters()
+
+        p1 = {"xi": 0.1, "tau": 70, "sources": [0.1, -0.3]}
+        p2 = {"xi": 0.2, "tau": 73, "sources": [-0.4, 0.1]}
+
+        ip.add_individual_parameters("idx1", p1)
+        ip.add_individual_parameters("idx2", p2)
+
+        ip_pytorch = ip.to_pytorch()
+
+        dict_test = {
+            "xi": torch.tensor([[0.1], [0.2]], dtype=torch.float32),
+            "tau": torch.tensor([[70], [73]], dtype=torch.float32),
+            "sources": torch.tensor([[0.1, -0.3], [-0.4, 0.1]], dtype=torch.float32)
+        }
+
+        self.assertEqual(ip_pytorch.keys(), dict_test.keys())
+        for k in dict_test.keys():
+            self.assertTrue((ip_pytorch[k] == dict_test[k]).all())
+
+
+    def test_from_pytorch(self):
+        """
+
+        """
+
+        ip_pytorch = {
+            "xi": torch.tensor([[0.1], [0.2]], dtype=torch.float32),
+            "tau": torch.tensor([[70], [73]], dtype=torch.float32),
+            "sources": torch.tensor([[0.1, -0.3], [-0.4, 0.1]], dtype=torch.float32)
+        }
+
+        ip = IndividualParameters.from_pytorch(ip_pytorch)
+
+        dict_test = {
+            0: {"xi": 0.1, "tau": 70., "sources": [0.1, -0.3]},
+            1: {"xi": 0.2, "tau": 73., "sources": [-0.4, 0.1]}
+        }
+
+        self.assertEqual(ip._indices, [0, 1])
+        self.assertEqual(ip._individual_parameters.keys(), dict_test.keys())
+        for k, v in dict_test.items():
+            for kk, vv in dict_test[k].items():
+                self.assertTrue(kk in ip._individual_parameters[k].keys())
+                if np.ndim(vv) == 0:
+                    self.assertAlmostEqual(ip._individual_parameters[k][kk], vv, delta=10e-8)
+                else:
+                    l2 = ip._individual_parameters[k][kk]
+                    for s1, s2 in zip(vv, l2):
+                        self.assertAlmostEqual(s1, s2, delta=10e-8)
+
+    def test_from_to_pytorch(self):
+        #TODO
+        return 0
+
+    def test_check_and_get_extension(self):
+        #TODO
+        return 0
+
+    def test_save_csv(self):
+        #TODO
+        return 0
+
+    def test_save_json(self):
+        #TODO
+        return 0
+
+    def test_load_csv(self):
+        #TODO
+        return 0
+
+    def test_load_json(self):
+        #TODO
+        return 0
+
+    def test_load_individual_parameters(self):
+        #TODO
+        return 0
+
+    def test_save_individual_parameters(self):
+        #TODO
+        return 0
+
+
+
+
+
+
+
+
+
+

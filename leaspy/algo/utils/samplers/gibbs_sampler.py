[FIX] Fix issue with MH & FastGibbs samplers due to possible broadcasting with scale and cover those samplers by functional fit tests

leaspy/algo/utils/samplers/gibbs_sampler.py
@@ -101,7 +101,13 @@ class GibbsSampler(AbstractSampler):
                 shape_adapted_std = (self.shape[0],)
             elif self.sampler_type == 'Metropolis-Hastings':
                 # Proposition variance is the same for all coordinates
-                shape_adapted_std = (1,)  # or empty tuple
+                shape_adapted_std = ()  # empty tuple -> we should not use (1,) because `ndindex` would not be correct
+
+            # <!> `scale` has to be of `shape_adapted_std`, otherwise there would be automatic broadcasting and `self.std` could not be of the intended shape! (for samplers other than Gibbs)
+            if scale.ndim > len(shape_adapted_std):
+                # we take the mean of groupped dimension in this case
+                scale_squeezed_dims = tuple(range(len(shape_adapted_std), scale.ndim))
+                scale = scale.mean(dim=scale_squeezed_dims)
 
             self.std = self.STD_SCALE_FACTOR_POP * scale * torch.ones(shape_adapted_std)
             self.acceptation_history = torch.zeros(self.acceptation_history_length, *shape_adapted_std)

tests/unit_tests/algo/samplers/test_gibbs_sampler.py
@@ -132,7 +132,7 @@ class SamplerTest(LeaspyTestCase):
         acceptation_for_draws = {
             'Gibbs': (cycle([torch.tensor([0., 0., 1., 1.])]*3 + [torch.tensor([0., 1., 0., 1.])]*2), torch.tensor([0., 2/5, 3/5, 1.])),
             'FastGibbs': (cycle([torch.tensor([0., 0., 1., 1.])]*3 + [torch.tensor([0., 1., 0., 1.])]*2), torch.tensor([0., 2/5, 3/5, 1.])),
-            'Metropolis-Hastings': (cycle([torch.tensor([1.])]*3 + [torch.tensor([0.])]*2), torch.tensor([3/5])),
+            'Metropolis-Hastings': (cycle([torch.tensor(1.)]*3 + [torch.tensor(0.)]*2), torch.tensor(3/5)),
         }
         for sampler, (acceptation_it, expected_mean_acceptation) in acceptation_for_draws.items():
             gsampler = GibbsSampler(self.leaspy.model.random_variable_informations()[var_name], n_patients,
@@ -150,7 +150,7 @@ class SamplerTest(LeaspyTestCase):
         acceptation_for_draws = {
             'Gibbs': (cycle([torch.tensor([[0., 0.], [0., 1.], [1., 1.]])]*3 + [torch.tensor([[0., 1.], [1., 0.], [0., 1.]])]*2), torch.tensor([[0., 2/5], [2/5, 3/5], [3/5, 1.]])),
             'FastGibbs': (cycle([torch.tensor([0., 0., 1.])]*3 + [torch.tensor([0., 1., 0.])]*2), torch.tensor([0., 2/5, 3/5])),
-            'Metropolis-Hastings': (cycle([torch.tensor([1.])]*3 + [torch.tensor([0.])]*2), torch.tensor([3/5])),
+            'Metropolis-Hastings': (cycle([torch.tensor(1.)]*3 + [torch.tensor(0.)]*2), torch.tensor(3/5)),
         }
         for sampler, (acceptation_it, expected_mean_acceptation) in acceptation_for_draws.items():
             gsampler = GibbsSampler(self.leaspy.model.random_variable_informations()[var_name], n_patients,
616b5c83fda7e54f79dbb190261b6f1dd04f84bc
Romain Girard <3794677-RomainGirard@users.noreply.gitlab.com>
2022-05-04 22:28:50 +0200

    [TESTS] Test data slicing and iteration

--- tests/unit_tests/io_tests/data/test_data.py
+++ tests/unit_tests/io_tests/data/test_data.py
@@ -1,9 +1,14 @@
+import enum
 from leaspy.io.data.data import Data
 
 from tests import LeaspyTestCase
 
 
 class DataTest(LeaspyTestCase):
+
+    def load_multivariate_data(self):
+        path_to_data = self.get_test_data_path('data_mock', 'multivariate_data.csv')
+        return Data.from_csv_file(path_to_data)
 
     def test_constructor_univariate(self):
         path_to_data = self.get_test_data_path('data_mock', 'univariate_data.csv')
@@ -23,8 +28,7 @@ class DataTest(LeaspyTestCase):
         self.assertEqual(individual.observations, [[0.2], [0.2], [0.3], [0.5]])
 
     def test_constructor_multivariate(self):
-        path_to_data = self.get_test_data_path('data_mock', 'multivariate_data.csv')
-        data = Data.from_csv_file(path_to_data)
+        data = self.load_multivariate_data()
         individual = data[3]
 
         self.assertEqual(data.iter_to_idx[0], '007_S_0041')
@@ -38,3 +42,31 @@ class DataTest(LeaspyTestCase):
         self.assertEqual(individual.idx, '130_S_0102')
         self.assertEqual(individual.timepoints, [71.3, 71.8])
         self.assertEqual(individual.observations, [[0.3, 0.4, 0.5], [0.4, 0.5, 0.6]])
+
+    def test_data_slicing(self):
+        data = self.load_multivariate_data()
+        start, stop = 1, 5
+        sub_data = data[start:stop]
+
+        individual_key = 3
+        individual = data[individual_key]
+        sub_individual = sub_data[individual_key - start]
+
+        self.assertEqual(data.headers, sub_data.headers)
+        self.assertEqual(data.dimension, sub_data.dimension)
+        self.assertEqual(data.cofactors, sub_data.cofactors)
+        self.assertEqual(sub_data.n_individuals, min(data.n_individuals, stop) - start)
+
+        self.assertEqual(individual.idx, sub_individual.idx)
+        self.assertEqual(individual.timepoints, sub_individual.timepoints)
+        self.assertEqual(individual.observations, sub_individual.observations)
+
+    def test_data_iteration(self):
+        data = self.load_multivariate_data()
+        for iter, individual in enumerate(data):
+            expected_individual = data[iter]
+            self.assertEqual(individual.idx, expected_individual.idx)
+            self.assertEqual(individual.timepoints, expected_individual.timepoints)
+            self.assertEqual(individual.observations, expected_individual.observations)
+            if iter > 4:
+                break

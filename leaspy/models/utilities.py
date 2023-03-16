598fba925b2eb93bf31a09f98c97f8ead5e7c3e9
Etienne Maheux <etienne.maheux@icm-institute.org>
2023-03-16 18:26:36 +0100

    [FORMAT] Black

--- leaspy/models/utilities.py
+++ leaspy/models/utilities.py
@@ -3,42 +3,42 @@ from leaspy.exceptions import LeaspyConvergenceError
 
 
 def compute_std_from_variance(
-        variance: torch.FloatTensor,
-        varname: str,
-        tol: float = 1e-5,
+    variance: torch.FloatTensor,
+    varname: str,
+    tol: float = 1e-5,
 ) -> torch.FloatTensor:
-        """
-        Check that variance is strictly positive and return its square root, otherwise fail with a convergence error.
+    """
+    Check that variance is strictly positive and return its square root, otherwise fail with a convergence error.
 
-        If variance is multivariate check that all components are strictly positive.
+    If variance is multivariate check that all components are strictly positive.
 
-        TODO? a full Bayesian setting with good priors on all variables should prevent such convergence issues.
+    TODO? a full Bayesian setting with good priors on all variables should prevent such convergence issues.
 
-        Parameters
-        ----------
-        variance : :class:`torch.Tensor`
-            The variance we would like to convert to a std-dev.
-        varname : str
-            The name of the variable - to display a nice error message.
-        tol : float
-            The lower bound on variance, under which the converge error is raised.
+    Parameters
+    ----------
+    variance : :class:`torch.Tensor`
+        The variance we would like to convert to a std-dev.
+    varname : str
+        The name of the variable - to display a nice error message.
+    tol : float
+        The lower bound on variance, under which the converge error is raised.
 
-        Returns
-        -------
-        torch.FloatTensor
+    Returns
+    -------
+    torch.FloatTensor
 
-        Raises
-        ------
-        :exc:`.LeaspyConvergenceError`
-        """
-        if (variance < tol).any():
-            raise LeaspyConvergenceError(
-                f"The parameter '{varname}' collapsed to zero, which indicates a convergence issue.\n"
-                "Start by investigating what happened in the logs of your calibration and try to double check:"
-                "\n- your training dataset (not enough subjects and/or visits? too much missing data?)"
-                "\n- the hyperparameters of your Leaspy model (`source_dimension` too low or too high? "
-                "`noise_model` not suited to your data?)"
-                "\n- the hyperparameters of your calibration algorithm"
-                )
+    Raises
+    ------
+    :exc:`.LeaspyConvergenceError`
+    """
+    if (variance < tol).any():
+        raise LeaspyConvergenceError(
+            f"The parameter '{varname}' collapsed to zero, which indicates a convergence issue.\n"
+            "Start by investigating what happened in the logs of your calibration and try to double check:"
+            "\n- your training dataset (not enough subjects and/or visits? too much missing data?)"
+            "\n- the hyperparameters of your Leaspy model (`source_dimension` too low or too high? "
+            "`noise_model` not suited to your data?)"
+            "\n- the hyperparameters of your calibration algorithm"
+        )
 
-        return variance.sqrt()
+    return variance.sqrt()

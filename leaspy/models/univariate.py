3e6c07d8e1810481e37f6845acf1bd4a29070df3
NicolasGensollen <nicolas.gensollen@gmail.com>
2023-08-23 14:52:05 +0200

    add linear and logistic subclasses and move initialization logic inside class

--- leaspy/models/univariate.py
+++ leaspy/models/univariate.py
@@ -1,19 +1,10 @@
-from leaspy.models.multivariate import MultivariateModel
-#from leaspy.models.noise_models import (
-#    BaseNoiseModel,
-#    BernoulliNoiseModel,
-#    GaussianScalarNoiseModel,
-#    AbstractOrdinalNoiseModel,
-#)
+from leaspy.models.multivariate import LogisticMultivariateModel, LinearMultivariateModel
 from leaspy.exceptions import LeaspyModelInputError
 from leaspy.utils.docs import doc_with_super
 
-# TODO refact? subclass or other proper code technique to extract model's concrete
-#  formulation depending on if linear, logistic, mixed log-lin, ...
-
 
 @doc_with_super()
-class UnivariateModel(MultivariateModel):
+class LogisticUnivariateModel(LogisticMultivariateModel):
     """
     Univariate (logistic or linear) model for a single variable of interest.
 
@@ -27,33 +18,44 @@ class UnivariateModel(MultivariateModel):
     Raises
     ------
     :exc:`.LeaspyModelInputError`
-        * If `name` is not one of allowed sub-type: 'univariate_linear' or 'univariate_logistic'
         * If hyperparameters are inconsistent
     """
-
-    SUBTYPES_SUFFIXES = {
-        'univariate_linear': '_linear',
-        'univariate_logistic': '_logistic'
-    }
-
     def __init__(self, name: str, **kwargs):
-
         if kwargs.pop('dimension', 1) not in {1, None}:
-            raise LeaspyModelInputError("You should not provide `dimension` != 1 for univariate model.")
-
+            raise LeaspyModelInputError(
+                "You should not provide `dimension` != 1 for univariate model."
+            )
         if kwargs.pop('source_dimension', 0) not in {0, None}:
-            raise LeaspyModelInputError("You should not provide `source_dimension` != 0 for univariate model.")
-
+            raise LeaspyModelInputError(
+                "You should not provide `source_dimension` != 0 for univariate model."
+            )
         super().__init__(name, dimension=1, source_dimension=0, **kwargs)
 
-    # def check_noise_model_compatibility(self, model: BaseNoiseModel) -> None:
-    #     """
-    #     Check compatibility between the model instance and provided noise model.
-    #     """
-    #     super().check_noise_model_compatibility(model)
-    #     if not isinstance(model, (GaussianScalarNoiseModel, BernoulliNoiseModel, AbstractOrdinalNoiseModel)):
-    #         raise LeaspyModelInputError(
-    #             f"The univariate model is only compatible with the following noise models: "
-    #             "'gaussian-scalar', 'bernoulli', 'ordinal', or 'ordinal-ranking'. "
-    #             f"You provided a {type(model).__name__}."
-    #         )
+
+@doc_with_super()
+class LinearUnivariateModel(LinearMultivariateModel):
+    """
+    Univariate (logistic or linear) model for a single variable of interest.
+
+    Parameters
+    ----------
+    name : :obj:`str`
+        The name of the model.
+    **kwargs
+        Hyperparameters of the model.
+
+    Raises
+    ------
+    :exc:`.LeaspyModelInputError`
+        * If hyperparameters are inconsistent
+    """
+    def __init__(self, name: str, **kwargs):
+        if kwargs.pop('dimension', 1) not in {1, None}:
+            raise LeaspyModelInputError(
+                "You should not provide `dimension` != 1 for univariate model."
+            )
+        if kwargs.pop('source_dimension', 0) not in {0, None}:
+            raise LeaspyModelInputError(
+                "You should not provide `source_dimension` != 0 for univariate model."
+            )
+        super().__init__(name, dimension=1, source_dimension=0, **kwargs)

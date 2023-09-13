93539554d831e4e5d1dce4f67fab8348d9d24547
ORTHOLAND Juliette <juliette.ortholand@icm-institute.org>
2023-09-13 15:11:46 +0200

    Add initialisation, need for the dataset structure for further tests

--- leaspy/models/univariate_joint.py
+++ leaspy/models/univariate_joint.py
@@ -60,7 +60,6 @@ class UnivariateJointModel(UnivariateModel):
         super().__init__(name, **kwargs)
         self.obs_models += (observation_model_factory('weibull-right-censored', nu = 'nu', rho = 'rho', xi = 'xi', tau = 'tau'),)
 
-
     def get_variables_specs(self) -> NamedVariables:
         """
         Return the specifications of the variables (latent variables, derived variables,
@@ -109,16 +108,6 @@ class UnivariateJointModel(UnivariateModel):
 
         )
 
-        if self.source_dimension >= 1:
-            # TODO: How to handle w in survival process
-            pass
-        else:
-            pass
-
-        # TODO: WIP
-        # variables_info.update(self.get_additional_ordinal_population_random_variable_information())
-        # self.update_ordinal_population_random_variable_information(variables_info)
-
         return d
 
     ##############################
@@ -150,14 +139,72 @@ class UnivariateJointModel(UnivariateModel):
         # TODO: find a way to prevent re-computation of orthonormal basis since it should not have changed (v0_collinear update)
         #self.update_MCMC_toolbox({'v0_collinear'}, realizations)
 
+    ##############################
+    ###      Initialisation    ###
+    ##############################
+
+    def estimate_initial_longitudinal_parameters(self, dataset) -> VariablesValuesRO:
+
+        # Hardcoded
+        XI_STD = .5
+        TAU_STD = 5.
+        NOISE_STD = .1
+
+        # Get dataframe
+        df = dataset.to_pandas().dropna(how='all').set_index(['ID', 'TIME']).sort_index()
+
+        # Make basic data checks
+        assert df.index.is_unique
+        assert df.index.to_frame().notnull().all(axis=None)
+        if model.features != df.columns.tolist():
+            raise LeaspyInputError(f"Features mismatch between model and dataset: {model.features} != {df.columns}")
+
+        # Guet mean time
+        t0, _ = compute_patient_time_distribution(df)
+
+        # Enforce values are between 0 and 1
+        values, _ = compute_patient_values_distribution(df)
+        values = values.clamp(min=1e-2, max=1 - 1e-2)  # always "works" for ordinal (values >= 1)
+
+        # Extract lopes and convert into parameters
+        slopes, _ = compute_patient_slopes_distribution(df)
+        v0_array = get_log_velocities(slopes, model.features)
+        g_array = torch.log(
+            1. / values - 1.)  # cf. Igor thesis; <!> exp is done in Attributes class for logistic models
+
+        # Create smart initialization dictionary
+        parameters = {
+                'g': g_array.squeeze(),
+                'v0': v0_array.squeeze(),
+                'tau_mean': t0,
+                'tau_std': torch.tensor(TAU_STD),
+                'xi_mean': torch.tensor(0.),
+                'xi_std': torch.tensor(XI_STD),
+            'noise_std' : torch.tensor(NOISE_STD)
+            }
+        return parameters
+
+    def estimate_initial_event_parameters(self, dataset) -> VariablesValuesRO:
+        wbf = WeibullFitter().fit(dataset.event_time,  # - dataset.timepoints[:,0],
+                                  dataset.event_bool)
+        parameters = {
+            'rho': torch.log(torch.tensor(wbf.rho_)),
+            'nu': -torch.log(torch.tensor(wbf.lambda_))
+        }
+        return parameters
 
     def get_initial_model_parameters(self, dataset: Dataset, method: str) -> VariablesValuesRO:
         """Get initial values for model parameters."""
 
-        # TODO - WIP (put back `initialize_parameters` function in this one, with possible utils functions in models.utilities?)
-        from leaspy.models.utils.initialization.model_initialization import initialize_parameters
-        params, obs_model_params = initialize_parameters(self, dataset, method=method)
-        # all parameters in one
-        params.update(obs_model_params)
+        # Estimate initial parameters from the data
+        params = self.estimate_initial_longitudinal_parameters(dataset)
+        params_event = self.estimate_initial_event_parameters(dataset)
+        params.update(params_event)
 
-        return params
+        # convert to float 32 bits & add a rounding step on the initial parameters to ensure full reproducibility
+        rounded_parameters = {
+            str(p): _torch_round(v.to(torch.float32))
+            for p, v in params.items()
+        }
+
+        return rounded_parameters
